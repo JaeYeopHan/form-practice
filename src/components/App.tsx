2518e8cc5e4c0367fe23f4eeeea1118735afcef6
JaeYeopHan <ljyhanll@gmail.com>
2020-01-03 17:50:12 +0900

    feat: Refactor FormContents component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import { FormCheckbox } from "@/components/shared/form/FormCheckbox";
 import { RootState } from "@/features";
-import { FORM, formActions, FormItem, formSelectors, formThunks } from "@/features/form";
+import { FORM, formActions, FormItem, formSelectors, formThunks, FormType } from "@/features/form";
 import { LOADING, LoadingState } from "@/features/loading";
 
 import { Button } from "./shared/Button";
-import { FormContents } from "./shared/form/FormContents";
+import { Empty } from "./shared/Empty";
+import { FormInput } from './shared/form/FormInput';
+import { FormRadio } from './shared/form/FormRadio';
+import { FormSelectBox } from './shared/form/FormSelectbox';
 import { FormTitle } from "./shared/form/FormTitle";
 import { FormWrapper } from "./shared/form/FormWrapper";
 import { Loading } from "./shared/Loading";
@@ -19,6 +23,7 @@ export default () => {
   const title = useSelector<RootState, string>(state => formSelectors.title(state[FORM]))
   const item = useSelector<RootState, FormItem>(state => formSelectors.currentItem(state[FORM]))
 
+  const handleUpdate = (value: string) => dispatch(formActions.updateAnswer({ [item.itemId]: value }))
   const handleNextClick = () => dispatch(formActions.toNext())
   const handleBackClick = () => dispatch(formActions.toPrev())
   const handleSubmitClick = () => console.log(`submit`)
@@ -27,16 +32,24 @@ export default () => {
     dispatch(formThunks.fetchFormData())
   }, [dispatch])
 
+  const FormComponent = {
+    [FormType.CheckBox]: FormCheckbox,
+    [FormType.Radio]: FormRadio,
+    [FormType.TextInput]: FormInput,
+    [FormType.SelectBox]: FormSelectBox,
+  }[item.formType] || Empty
+
   if (loading[FORM]) {
     return <Loading />
   }
 
   return (
     <Main>
+
       <Title>{title}</Title>
       <FormWrapper>
         <FormTitle>{item.title}</FormTitle>
-        <FormContents {...item} />
+        <FormComponent onUpdate={handleUpdate} options={item.options} />
       </FormWrapper>
       <Button onClick={handleBackClick}>Back</Button>
       <Button onClick={handleNextClick}>Next</Button>

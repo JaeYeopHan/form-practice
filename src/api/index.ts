73944a83369971f6c6255c9c4532615722035817
JaeYeopHan <ljyhanll@gmail.com>
2020-01-03 17:58:16 +0900

    feat: Add setFormAnswer function

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,3 +10,13 @@ export interface FormDataResponse {
 export const getFormData = (): Promise<FormDataResponse> => {
   return Promise.resolve(mock);
 };
+
+export interface SetFormDataRequest {
+  id: number;
+  items: { id: number; answer: string }[];
+}
+
+export const setFormData = (data: SetFormDataRequest): Promise<boolean> => {
+  console.log(data);
+  return Promise.resolve(true);
+};
